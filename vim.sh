9c5d4e41696977d6fc58da155826cc0b937152eb
Nazri Ramliy <ayiehere@gmail.com>
2013-11-09 11:36:19 +0800

    vim: with-x only on linux

--- vim.sh
+++ vim.sh
@@ -3,11 +3,16 @@ d=`dirname $0`
 vcs=hg
 . $d/common.sh
 ${vcs}_exit_if_no_changes Vim
+os=`uname`
+with_x=
+if [ "$os" = Linux ]; then
+	with_x=--with-x
+fi
 $linefan CC=clang ./configure \
     --enable-multibyte \
     --enable-pythoninterp=yes \
     --prefix=${1:-/usr/local} \
-    --with-x \
+    $with_x \
     --with-features=huge &&
 $linefan make -f Makefile -j3 install &&
 ${vcs}_update_lastbuilt
