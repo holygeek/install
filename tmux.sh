35471945d338dafefbc4b5a1987062a77674c1fe
Nazri Ramliy <ayiehere@gmail.com>
2013-11-09 11:21:44 +0800

    Enable tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -3,6 +3,6 @@ d=`dirname $0`
 vcs=git
 . $d/common.sh
 ${vcs}_exit_if_no_changes Tmux
-echo ./configure --prefix=/usr/local &&
-echo make -j2 install &&
+./configure --prefix=/usr/local &&
+make -j2 install &&
 ${vcs}_update_lastbuilt
