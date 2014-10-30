a79a45a3cbf9e22272bb4eb80510603161a38a8b
Nazri Ramliy <ayiehere@gmail.com>
2014-10-30 09:23:36 +0800

    tmux: run autogen.sh if necessary

--- tmux.sh
+++ tmux.sh
@@ -3,6 +3,7 @@ d=`dirname $0`
 vcs=git
 . $d/common.sh
 ${vcs}_exit_if_no_changes Tmux
+if [ ! -x configure ]; then ./autogen.sh; fi
 $linefan ./configure --prefix=/usr/local &&
 $linefan make -j2 install &&
 ${vcs}_update_lastbuilt
