6001a9c8e936ee0392b47d55bc15cd16a2bc4352
Nazri Ramliy <ayiehere@gmail.com>
2021-08-09 14:58:52 +0800

    tmux.sh: Use utf8proc on macOS

--- tmux.sh
+++ tmux.sh
@@ -4,6 +4,19 @@ vcs=git
 . $d/common.sh
 ${vcs}_exit_if_no_changes Tmux
 if [ ! -x configure ]; then ./autogen.sh; fi
-$linefan ./configure --prefix=/usr/local &&
+
+#configure:         macOS library support for Unicode is very poor,
+#configure:         particularly for complex codepoints like emojis;
+#configure:         to use these correctly, configuring with
+#configure:         --enable-utf8proc is recommended. To build
+#configure:         without anyway, use --disable-utf8proc
+os=`uname`
+configure_opt=
+if [ "$os" = Darwin ]; then
+	# Need utf8proc, brew install utf8proc
+	configure_opt='--enable-utf8proc'
+fi
+
+$linefan ./configure --prefix=/usr/local $configure_opt &&
 $linefan make -j2 install &&
 ${vcs}_update_lastbuilt
