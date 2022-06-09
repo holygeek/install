a07cf33bb2b08c90b21d9fca46a549900f4a2bd3
Nazri Ramliy <ayiehere@gmail.com>
2022-06-09 18:09:03 +0800

    git.sh: Set LIBPCREDIR on mac

--- git.sh
+++ git.sh
@@ -2,10 +2,17 @@
 d=`dirname $0`
 vcs=git 
 . $d/common.sh
+
+libpcredir=
+case "$mach" in
+	Darwin) libpcredir=LIBPCREDIR=/opt/homebrew;;
+esac
+
 test -n "$INSTALL" && ${vcs}_exit_if_no_changes Git
 $linefan make PREFIX=${prefix:-/usr/local} \
     CC=clang \
     USE_LIBPCRE=YesPlease \
+    $libpcredir \
     NO_GETTEXT=YesPlease \
     $INSTALL -j$parallel &&
     test -n "$INSTALL" &&
